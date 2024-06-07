134a0a0b9d6fcb0ac2bdc1f7ff6bf1e2a7bc96d8
Kristóf Nyikes <kristof.nyikes@gmail.com>
2024-06-07 11:45:26 +0200

    Add connection to mongoDB

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import products from './data/products.js';
+import dotenv from 'dotenv';
+dotenv.config();
+import connectDB from './config/db.js';
+
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+
+app.get('/api/products', (req, res) => {
+    res.send('API is running...');
+  });
+
+app.listen(port, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+);
--- frontend/src/Screens/ProductScreen.jsx
+++ frontend/src/Screens/ProductScreen.jsx
@@ -2,11 +2,20 @@ import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/Rating';
-import products from '../products';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 const ProductScreen = () => {
+  const [product, setProduct] = useState({})
   const { id: productId } = useParams();
-  const product = products.find((p) => p._id === productId);
+
+  useEffect(() => {
+    const fetchProducts = async () =>{
+      const {data} = await axios.get(`/api/products/${productId}`)
+      setProduct(data);
+    }
+    fetchProducts();
+  },[productId])
 
   return (
     <>
